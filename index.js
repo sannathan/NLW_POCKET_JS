628872b39a9557c2759e059ffe3fd79ef8c2cb9a
Nathan Barbosa <nathansantos1713@gmail.com>
2024-09-10 14:07:59 -0300

    fundamentos 003

---
+++ index.js
@@ -0,0 +1,12 @@
+let meta = {
+    value: 'ler um livro por mês',
+    checked: false,    
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
